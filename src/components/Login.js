1ba654ebe85293e4bd032912eff70205188805d3
Prakash Dey <91982512+prakash-dey@users.noreply.github.com>
2024-11-13 23:27:21 +0530

    changes

---
+++ src/components/Login.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './Header'
+
+const Login = () => {
+  return (
+    <div className='h-full'>
+        <Header/>
+        <div className='w-full h-full'>
+            <img className="h-svh" src='https://assets.nflxext.com/ffe/siteui/vlv3/151f3e1e-b2c9-4626-afcd-6b39d0b2694f/web/IN-en-20241028-TRIFECTA-perspective_bce9a321-39cb-4cce-8ba6-02dab4c72e53_small.jpg' alt='bg_img'/>
+        </div>
+    </div>
+  )
+}
+
+export default Login
